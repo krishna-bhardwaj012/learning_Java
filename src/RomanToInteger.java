bb87597c7a316189e294f5d8154a32525133872f
krishna-bhardwaj012 <sachinkumar221155@gmail.com>
2025-07-18 23:05:26 +0530

    Initial Java project upload

---
+++ src/RomanToInteger.java
@@ -0,0 +1,42 @@
+public class RomanToInteger {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.romanToInt("MCMXCIV")); // Output: 1994
+    }
+}
+
+class Solution {
+    public int romanToInt(String s) {
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int current = getValue(s.charAt(i));
+
+            if (i + 1 < s.length()) {
+                int next = getValue(s.charAt(i + 1));
+                if (current < next) {
+                    total -= current;
+                    continue;
+                }
+            }
+
+            total += current;
+        }
+
+        return total;
+    }
+
+    private int getValue(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+}
